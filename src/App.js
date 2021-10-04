8fa054d65ef8210c3ba96779e8357471cd23b712
Tauanny Furlanetto <tauannyvfs@gmail.com>
2021-10-04 12:11:45 -0300

    Chamar a API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import apiGitHub from "./services/apiGithub.js";
+import {useEffect} from "react";
 
 function App() {
+  useEffect(()=>{
+    apiGitHub.get("users/tauannyFurlanetto").then(response=>{response.data}).then(response=>console.log(response)).catch(error=>console.log(error));
+  }, [ ])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ } className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
